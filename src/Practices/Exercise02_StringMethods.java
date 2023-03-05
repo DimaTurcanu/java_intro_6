9855384d6c348c120bbf88c0979ea8afa7ca1e3f
Dima Turcanu <turcanu.dumitru20@gmail.com>
2023-03-05 13:25:06 -0600

    added branch

--- src/Practices/Exercise02_StringMethods.java
+++ src/Practices/Exercise02_StringMethods.java
@@ -5,9 +5,14 @@ import utilities.ScannerHelper;
 public class Exercise02_StringMethods {
     public static void main(String[] args) {
         String str = ScannerHelper.getString();
-        if (str.length()<3) System.out.println("Length is less than 3");
-        if (str.length()>=3)
-            if (str.length()%2 != 0) System.out.println(str.charAt(str.length()/2));
-        else System.out.println(str.substring(str.length() / 2 - 1, str.length() / 2 + 1));
+
+
+        if (str.length()<3) System.out.println("");
+        if (str.toLowerCase().substring(0,2).contains("a"))
+            System.out.println("a" + str.substring(2));
+       else  if (str.toLowerCase().substring(0,2).contains("b"))
+            System.out.println("b" + str.substring(2));
+        System.out.println(str.substring(2));
+
     }
 }
--- src/gitDemo/Demo.java
+++ src/gitDemo/Demo.java
@@ -3,6 +3,8 @@ package gitDemo;
 public class Demo {
     public static void main(String[] args) {
         String fName = "Dima";
+        String lName = "Turcanu";
 
+        System.out.println(fName + lName);
     }
 }
