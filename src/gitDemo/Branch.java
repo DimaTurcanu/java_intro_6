20fb0b179da86c8bc10ce6dfd8d8f7544a1a3b44
Dima Turcanu <turcanu.dumitru20@gmail.com>
2023-03-05 13:49:55 -0600

    2 new lines

--- src/gitDemo/Branch.java
+++ src/gitDemo/Branch.java
@@ -4,5 +4,7 @@ public class Branch {
     public static void main(String[] args) {
         System.out.println("");
         // task02 added new line
+        System.out.println("");
+        System.out.println("");
     }
 }
